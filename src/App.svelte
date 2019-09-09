51d653b1bb5d011f50f6835d86d873ddcc42fb25
nikolaev <anikolaev@themeetgroup.com>
2019-09-10 01:54:15 +0300

    svelte starter config | some components

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+	import Day from './Day.component.svelte';
+	import Savings from './Savings.component.svelte';
+	import Overall from './Overall.svelte';
+
+	export let name;
+</script>
+
+<style>
+	h1 {
+		color: purple;
+	}
+</style>
+
+<Day />
+<Savings />
+<Overall />
---
+++ src/Savings.component.svelte
@@ -0,0 +1,11 @@
+<script>
+	import TextNumber from './TextNumber.svelte';
+</script>
+
+<section>
+	<TextNumber text='Savings' num='60' />
+	<article>
+		<button>Save</button>
+		<button>Withdraw</button>
+	</article>
+</section>
