79a27deed2035c2c29c1cfb12b2a6cea0e654f57
nikolaev <anikolaev@themeetgroup.com>
2019-09-10 03:18:11 +0300

    component: block: Modal: create shape for modals

---
+++ src/components/blocks/Modal.svelte
@@ -0,0 +1,27 @@
+<script>
+	export let id='modal';
+</script>
+
+<style>
+	.background {
+		position: fixed;
+		background-color: rgba(0, 0, 0, 0.50);
+		top: 0;
+		right: 0;
+		bottom: 0;
+		left: 0;
+		z-index: 999;
+		visibility: hidden;
+		opacity: 0;
+		transition: all 0.3s;
+	}
+
+	:target {
+    	visibility: visible;
+		opacity: 1;
+	}
+</style>
+
+<section class='background' {id}>
+	<slot>Empty Modal id:{id}</slot>
+</section>
