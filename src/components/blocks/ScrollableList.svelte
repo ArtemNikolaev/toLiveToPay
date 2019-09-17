82f55fbd354f993df8d4fe425eea0436fada7751
nikolaev <anikolaev@themeetgroup.com>
2019-09-17 13:09:20 +0300

    add: list component

---
+++ src/components/blocks/ScrollableList.svelte
@@ -0,0 +1,19 @@
+<style>
+	.container {
+		height: 100%;
+		overflow: hidden;
+	}
+
+	.content{
+		height: 100%;
+		display: flex;
+		flex-direction: column;
+		overflow-y: scroll;
+	}
+</style>
+
+<div class='container'>
+	<div class="content">
+		<slot name='list' />
+	</div>
+</div>
