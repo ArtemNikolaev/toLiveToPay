489d2c83a09f5d3ffb55c03e0612536f61c4e08c
nikolaev <anikolaev@themeetgroup.com>
2019-09-19 00:44:18 +0300

    add: move updateCategories logic to separate file

--- src/components/widgets/ModalCategories.svelte
+++ src/components/widgets/ModalCategories.svelte
@@ -1,26 +1,11 @@
 <script>
-	import AddCategory from '../blocks/AddCategory.svelte';
-	import {
-		categoriesStore,
-		addCategoryToStore,
-		removeCategoryFromStore,
-	} from '../../stores/categoriesStore';
+	import CategoryAdd from '../blocks/CategoryAdd.svelte';
+	import CategoryUpdate from '../blocks/CategoryUpdate.svelte';
 	import Modal from '../blocks/Modal.svelte';
 	import Button from '../elements/Button.svelte';
-	import Input from '../elements/Input.svelte';
 	import ScrollableList from '../blocks/ScrollableList.svelte';
-	import {
-		closeModal,
-		modalsNames
-	}			from '../../models/modalManager';
-
-	export let categories = [];
-
-	function removeCategory(cat) {
-		categories.splice(categories.indexOf(cat), 1);
-
-		categories = [...categories];
-	}
+	import { categoriesStore } from '../../stores/categoriesStore';
+	import { closeModal, modalsNames } from '../../models/modalManager';
 </script>
 
 <style>
@@ -32,36 +17,17 @@
 		list-style-type: none;
 		padding: 0;
 	}
-
-	li {
-		display: grid;
-		grid-template-columns: 3fr 1fr 1fr;
-		align-items: center;
-		justify-items: center;
-	}
 </style>
 
 <Modal id={modalsNames.categories} header="Categories">
 	<section slot='body'>
-		<AddCategory />
+		<CategoryAdd />
 
 		<article class='categories'>
 			<ScrollableList>
 				<ul slot='list'>
-					{#each $categoriesStore as $category}
-						<li>
-							{#if !$category.edit}
-								<span>{JSON.stringify($category)}</span>
-							{:else}
-								<Input type='text' bind:value={$category.name}/>
-							{/if}
-
-							<Button txt='✎' func={() => $category.edit = !$category.edit}/>
-							<Button
-								txt='🗑'
-								func={() => removeCategoryFromStore($category.id)}
-							/>
-						</li>
+					{#each $categoriesStore as category}
+						<CategoryUpdate {category} />
 					{/each}
 				</ul>
 			</ScrollableList>
--- src/stores/categoriesStore.js
+++ src/stores/categoriesStore.js
@@ -1,6 +1,27 @@
+import { readable, writable } from 'svelte/store';
 import dbConnection from '../models/indexedDBInit';
 
 let db;
+let set;
+
+/* Dummy Data */
+	let id = 0;
+	const categories = [];
+	categories[id] = {id, name: 'lunch'};
+	categories[++id] = {id, name: 'shop'};
+	categories[++id] = {id, name: 'uber'};
+
+	function categoriesMapper () {
+		const storeCategories = categories.map(
+			category => Object.assign({}, category, {edit: false}),
+		);
+
+		return storeCategories;
+	}
+
+/* /Dummy Data */
+
+export const categoriesStore = readable(categoriesMapper(), setFunc => set = setFunc);
 
 const unsubscribe = dbConnection.subscribe((connection) => {
 	if (!connection) return;
@@ -11,8 +32,22 @@ const unsubscribe = dbConnection.subscribe((connection) => {
 })
 
 export function addCategory (name) {
+	console.log('addCategory: ', { name });
+
 	// TODO: name validation
 	// TODO: saving to DB
+}
 
-	console.log('addCategory: ', name);
+export function deleteCategory (id) {
+	console.log('deleteCategory:', { id });
+
+	// TODO: removing from DB
+}
+
+export function updateCategory(id, name) {
+	console.log('updateCategory: ', { id, name });
+
+	// TODO: update in DB
+
+	set(categoriesMapper());
 }
