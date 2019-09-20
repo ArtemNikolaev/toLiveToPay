c8d0b22d1830d2f0c1df0ba895e9383f4cae971f
nikolaev <anikolaev@themeetgroup.com>
2019-09-21 02:00:50 +0300

    Sentry add release version

--- src/utils/trirdparty.js
+++ src/utils/trirdparty.js
@@ -1,6 +1,10 @@
 import * as Sentry from '@sentry/browser';
 import { trirdparty } from '../../etc/config';
+import { version } from '../../package.json';
 
 export function init() {
-	Sentry.init({ dsn: trirdparty.sentry });	
+	Sentry.init({
+		dsn: trirdparty.sentry,
+		release: version,
+	});	
 }
