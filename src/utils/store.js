ed82d6995b459adbf635d07107bad7e1ac0a1c2e
nikolaev <anikolaev@themeetgroup.com>
2019-09-27 01:00:57 +0300

    set redux store as a default export

--- src/utils/store.js
+++ src/utils/store.js
@@ -5,49 +5,47 @@ import * as categories from './categoriesHelper';
 //
 import { setSettings } from '../stores/dataStore';
 
-export const actions = {
-	SET_SETTINGS: 'SET_SETTINGS',
-};
-
 function initialState() {
-	const state = {
-		settings: JSON.parse(localStorage.getItem('settings')) || settings,
-		categories: categories.initial(),
-	};
+  const state = {
+    settings: JSON.parse(localStorage.getItem('settings')) || settings,
+    categories: categories.initial(),
+  };
 
-	return JSON.parse(JSON.stringify(state));
+  return JSON.parse(JSON.stringify(state));
 }
 
 function settingsReducer (state, settings) {
-	return Object.assign({}, state, { settings });
+  return Object.assign({}, state, { settings });
 }
 
 function reducer(state, action) {
   state = Object.assign({}, state);
 
-	switch (action.type) {
-		case actions.SET_SETTINGS:
-			return settingsReducer(state, action.payload);
+  switch (action.type) {
+    case 'SAVE_SETTINGS':
+      return settingsReducer(state, action.payload);
     case 'ADD_CATEGORY':
       return categories.add(state, action.payload);
     case 'UPDATE_CATEGORY':
       return categories.update(state, action.payload);
     case 'REMOVE_CATEGORY':
       return categories.remove(state, action.payload);
-		default:
-			return state;
-	}
+    default:
+      return state;
+  }
 }
 
-export let store = createStore(reducer, initialState());
+const store = createStore(reducer, initialState());
 
 // TODO: remove after all checks
 store.subscribe(() => {
-	const state = store.getState();
+  const state = store.getState();
 
-	console.log('subscribe: ', state);
+  console.log('subscribe: ', state);
 
-	setSettings(state.settings);
+  setSettings(state.settings);
 
-	categories.save(state.categories);
+  categories.save(state.categories);
 });
+
+export default store;
