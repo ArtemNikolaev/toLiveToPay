6c27fa1f2c1c3a1b68694fbf20bd71631fa3e111
nikolaev <anikolaev@themeetgroup.com>
2019-09-26 18:17:23 +0300

    remove token functionality

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import App from './components/App.svelte';
-import { exist } from './models/token';
 import { init } from './utils/trirdparty';
 // TODO: withdraw can't be more than all savings value
 // TODO: savings can't be more than all available money
@@ -8,9 +7,6 @@ init();
 
 const app = new App({
 	target: document.body,
-	props: {
-		token: exist()
-	}
 });
 
 export default app;
