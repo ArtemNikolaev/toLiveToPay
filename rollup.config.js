a1a115c1fce731c8fa92211c48286238ea2b7f4c
nikolaev <anikolaev@themeetgroup.com>
2019-09-26 19:56:57 +0300

    add redux and run setting through it

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
+import replace from 'rollup-plugin-replace';
 import json from 'rollup-plugin-json';
 
 const production = !process.env.ROLLUP_WATCH;
@@ -16,6 +17,9 @@ export default {
 		file: 'public/bundle.js'
 	},
 	plugins: [
+		replace({
+            'process.env.NODE_ENV': JSON.stringify( 'production' )
+        }),
 		json({
       		exclude: [ 'node_modules/**' ],
 			preferConst: true,
