f71d97298eda682169b847bc606e7c681db28a52
Artem Nikolaev <artem@nikolaev.by>
2021-12-22 19:16:37 +0300

    added base style for Settings

--- src/app/overall-left/overall-left.component.ts
+++ src/app/overall-left/overall-left.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {MatDialog} from "@angular/material/dialog";
+import {SettingsComponent} from "../settings/settings.component";
 
 @Component({
   selector: 'overall-left',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OverallLeftComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {
   }
 
+  openSettings() {
+    this.dialog.open(SettingsComponent);
+  }
 }
