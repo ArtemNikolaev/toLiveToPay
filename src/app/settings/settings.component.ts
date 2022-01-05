8f05257361c33122f226848368689e33837371b9
Artem Nikolaev <artem@nikolaev.by>
2022-01-05 17:34:12 +0300

    `settings` state and storages

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { SettingsService } from '../settings.service';
 import { Store } from '@ngrx/store';
@@ -6,13 +6,14 @@ import * as dayjs from 'dayjs';
 import { Settings } from '../models/settings.model';
 import { update } from '../state/settings/settings.actions';
 import { selectSettings } from '../state/selectors';
+import {first} from "rxjs";
 
 @Component({
   selector: 'settings',
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.css']
 })
-export class SettingsComponent implements OnInit {
+export class SettingsComponent {
   settings$ = this.store.select(selectSettings);
   settingsForm = new FormGroup({
     amount : new FormControl('0'),
@@ -26,22 +27,13 @@ export class SettingsComponent implements OnInit {
     private store: Store,
     private settingsService: SettingsService
   ) {
-    this.settings$.subscribe((value: Settings) => {
+    this.settings$.pipe(first()).subscribe((value: Settings) => {
       this.settingsForm.setValue(value);
     })
   }
 
-  ngOnInit(): void {
-    this.settingsService.get().subscribe(
-      (settings: Settings | null) => {
-        if (!settings) return;
-
-        this.store.dispatch(update({payload: settings}))
-      }
-    );
-  }
-
   onSubmit() {
-    this.store.dispatch(update(this.settingsForm.value))
+    console.log(this.settingsForm.value);
+    this.store.dispatch(update({payload: this.settingsForm.value}))
   }
 }
--- src/app/state/settings/settings.reducer.ts
+++ src/app/state/settings/settings.reducer.ts
@@ -3,15 +3,26 @@ import {Action, createReducer, on} from '@ngrx/store';
 import {InputDate, Settings} from '../../models/settings.model';
 import { SettingsPayload, update} from './settings.actions';
 
-export const initialState: Settings = {
+const defaultData: Settings = {
   amount: 0,
   beginDate: dayjs().format('YYYY-MM-DD') as InputDate,
   endDate: dayjs().add(1, 'day').format('YYYY-MM-DD') as InputDate,
 };
 
+function generateInitialState(): Settings {
+  try {
+    return JSON.parse(localStorage.getItem('settings') || '') as Settings;
+  } catch (e) {
+    return defaultData;
+  }
+}
+
+export const initialState: Settings = generateInitialState();
+
 interface SettingsReducer extends  Action, SettingsPayload {}
 
 function updateReducer(state: Settings, props: SettingsReducer) {
+
   return props.payload;
 }
 
