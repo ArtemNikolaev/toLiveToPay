50c6b1f413c9b12422f1d5ca53b0c87c9d58bbf3
Artem Nikolaev <artem@nikolaev.by>
2022-01-06 12:24:17 +0300

    more informative spends

--- src/app/today-spends/today-spends.component.ts
+++ src/app/today-spends/today-spends.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
 import {selectInBudgetSpends} from "../state/selectors/inBudgetSpends.selector";
+import * as dayjs from "dayjs";
 
 @Component({
   selector: 'today-spends',
@@ -8,11 +9,28 @@ import {selectInBudgetSpends} from "../state/selectors/inBudgetSpends.selector";
   styleUrls: ['./today-spends.component.css']
 })
 export class TodaySpendsComponent {
+  spends : any = [];
   spends$ = this.store.select(selectInBudgetSpends);
   displayedColumns: string[] = ['time', 'sum', 'category', 'description'];
 
   constructor(
     private store: Store,
-  ) { }
+  ) {
+    this.spends$.subscribe(
+      data => {
+        this.spends = [];
+        data.forEach(el => {
+          this.spends.push({
+            date: el.date,
+            sum: el.sum,
+            time: dayjs.unix(el.time).format('hh:mm'),
+            category: el.category,
+            description: el.description,
+
+          })
+        })
+      }
+    )
+  }
 
 }
