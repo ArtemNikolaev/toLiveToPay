9b746fa40b25df8ebf9b5b6127ed4f7344993750
Artem Nikolaev <artem@nikolaev.by>
2022-01-21 13:20:19 +0300

    router and links to spends page and main page #67 #66

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {MainPageComponent} from "./components/main-page/main-page.component";
+import {SpendsComponent} from "./components/spends/spends.component";
+
+const routes: Routes = [
+  { path: 'spends', component: SpendsComponent },
+  { path: '**', component: MainPageComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
